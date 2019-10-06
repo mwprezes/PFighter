7342397964f6c08e905143569c783057866559ac
Nordtraveller <OlgierdJaros@gmail.com>
2019-10-06 18:33:32 +0200

    Conversion to c++ project

---
+++ Source/PFighter/PFighter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PFighter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PFighter, "PFighter" );
