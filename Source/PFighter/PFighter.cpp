6e493d8f724a0c2f0e80a5adaf9ef6574de9ac61
Nordtraveller <OlgierdJaros@gmail.com>
2020-03-07 10:59:21 +0100

    Clean project for blueprints

--- Source/PFighter/PFighter.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PFighter.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PFighter, "PFighter" );
